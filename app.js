52b3a0a8adac57b066aefacf7ca13f27171bcaa5
EndaMolloy <enda.molloy.1@gmail.com>
2018-02-11 18:26:00 +0000

    first serving of index.html

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
 require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
 const yargs = require('yargs');
 
 const weather = require('./controllers/weather');
 
-const argv = yargs
-  .options({
-    a: {
-      demand: true,
-      alias: 'address',
-      describe: 'get your weather data',
-      string: true
-    }
-  })
-  .help()
-  .alias('help', 'h')
-  .argv;
+const app = express();
 
-weather.getWeatherData(argv.a)
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', require('./routes/index'));
+
+// const argv = yargs
+//   .options({
+//     a: {
+//       demand: true,
+//       alias: 'address',
+//       describe: 'get your weather data',
+//       string: true
+//     }
+//   })
+//   .help()
+//   .alias('help', 'h')
+//   .argv;
+//
+// weather.getWeatherData(argv.a)
+
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`Server started listening on port ${port}!`));
