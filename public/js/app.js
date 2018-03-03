85b93da0c34fb010ce64981ac94e375c4aa3f3db
EndaMolloy <enda.molloy.1@gmail.com>
2018-03-03 20:37:20 +0000

    dropdown menu populated with past searches

--- public/js/app.js
+++ public/js/app.js
@@ -3,20 +3,52 @@
   if(localStorage.pastSearches){
     console.log("I've got old searches");
 
-  //save search address in local storage
-  // var pastSearches = [];
-  // localStorage.setItem("pastSearches", JSON.stringify(pastSearches));
 
-  let pastSearches = JSON.parse(localStorage.pastSearches)
+    let pastSearches = JSON.parse(localStorage.pastSearches)
 
-  if(pastSearches.indexOf(localStorage.address)== -1){
-    pastSearches.unshift(localStorage.address);
-    if(pastSearches.length > 3)
-      pastSearches.pop();
+    if(pastSearches.indexOf(localStorage.address)== -1){
+      pastSearches.unshift(localStorage.address);
+      if(pastSearches.length > 4)
+        pastSearches.pop();
 
-    localStorage.pastSearches = JSON.stringify(pastSearches);
+      localStorage.pastSearches = JSON.stringify(pastSearches);
+    }
+
+    populatePastSearchesMenu();
+  }else{
+      var pastSearches = [];
+      pastSearches.unshift(localStorage.address);
+      localStorage.setItem("pastSearches", JSON.stringify(pastSearches));
+
   }
+
+  // Click on a close button to hide the current list item
+  var close = document.getElementsByClassName("close");
+  var i;
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+
+      //remove dividers
+      if(div.nextElementSibling){
+        div.nextElementSibling.parentNode.removeChild(div.nextElementSibling);
+      }
+      else if (div.previousSibling) {
+        div.previousSibling.parentNode.removeChild(div.previousSibling);
+      }
+
+      //remove the address
+      div.parentNode.removeChild(div);
+
+      //if no more addresses then remove the menu <ul>
+      if(close.length == 0){
+        var elem = document.getElementById("search-menu");
+        elem.parentNode.removeChild(elem);
+      }
+
+    }
   }
+
 
 
   var navbarSearch = document.getElementById("navbar-search");
@@ -24,13 +56,66 @@
   navbarSearch.addEventListener("blur", rmFormatSearch, true);
 
   function focusSearch() {
-    document.getElementById("navbar-search").style.borderBottom = "white solid 1px";
     document.getElementById("navbar-btn").style.visibility = "visible";
   }
 
   function rmFormatSearch() {
     document.getElementById("navbar-btn").style.visibility = "hidden";
-    document.getElementById("navbar-search").style.borderBottom = "";
+  }
+
+  //dynamically add search locaiton from local storage to dropdown menu
+  function populatePastSearchesMenu(){
+
+    var pastSearches = JSON.parse(localStorage.pastSearches);
+    var currentSearch = JSON.parse(localStorage.address);
+
+    // var navbar = document.getElementById("navbar-search");
+    // navbar.setAttribute("")
+
+    //Create UL element and set id name
+    var menu = document.createElement("UL");
+    menu.setAttribute("id", "search-menu");
+    menu.className = "dropdown-menu";
+
+    document.getElementById("search-form").appendChild(menu);
+    console.log(menu);
+
+    var options = document.getElementById("search-menu");
+    var length = pastSearches.length;
+    for (var i = 0; i < length; i++) {
+      var address = JSON.parse(pastSearches[i])
+
+      if(localStorage.address !== pastSearches[i]){
+        var li = document.createElement("li");
+        var link = document.createElement("a");
+        var text = document.createTextNode(address.address_title);
+        link.appendChild(text);
+        link.href = `http://localhost:5000/forecast/${address.lat},${address.lng}/${address.address_title}`;
+        li.appendChild(link);
+
+        //add remove button
+        var span = document.createElement("SPAN");
+        var txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+        li.appendChild(span);
+
+        options.appendChild(li);
+
+        if (i < length - 1) {
+          var newDiv = document.createElement("div");
+          newDiv.className = "dropdown-divider";
+          options.appendChild(newDiv);
+        }
+      }
+
+    }
+
+    //prevent the menu from toggling when the user clicks on delete
+    //TODO should close when user clicks on address.
+    document.getElementById('search-menu').addEventListener('click', function (event) {
+      event.stopPropagation();
+    });
   }
 
 })();
